821946a40a40b9787ae2149688d71085231c7015
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-04 22:56:05 -0500

    Modify loader to support Sequelize and Postgres

--- loader/index.js
+++ loader/index.js
@@ -1,44 +1,72 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const fs = require('fs');
-const AWS = require('aws-sdk');
-
-AWS.config.update({
-  region: 'us-east-2',
-});
+const init = require('../functions/models/index');
+const ProgressBar = require('progress');
 
 async function run() {
-  const client = new AWS.DynamoDB.DocumentClient();
-
+  const sequelize = init();
+  
   let content = JSON.parse(fs.readFileSync('data/complete.json', 'utf8'));
 
+  const models = sequelize.models;
+
+  const bar = new ProgressBar(':bar :current | :total', { total: content.length });
+
   for (let i = 0; i < content.length; i++) {
-    const id = `${content[i].term}:${content[i].subj}${content[i].num}.${content[i].sect}`;
-    
-    content[i].id = id;
+    await saveItem(content[i], i, models);
 
-    const item = content[i];
+    bar.tick();
+  }
+}
 
-    const params = {
-      TableName: 'utd_grades_courses',
-      Item: {
-          'id':  item.id,
-          'semester': item.term,
-          'professor': item.prof,
-          'prefix': item.subj,
-          'number': item.num,
-          'section': item.sect,
-          'grades': item.grades
-      }
-    };
-    
-    client.put(params, function(err, data) {
-      if (err) {
-        console.error('Unable to add', JSON.stringify(err, null, 2));
-      } else {
-        console.log('PutItem succeeded:', item.id);
-      }
+async function saveItem(item, i, models) {
+  try {
+    // Find or create semester
+    const term = item.term.split(' ');
+    const term_year = parseInt(term[0]);
+    const term_term = term[1].toLowerCase();
+
+    const [semester, semester_created] = await models.semester.findOrCreate({
+      where: {
+        year: term[0],
+        type: term_term,
+      },
     });
+
+    // Find or create professor
+    const prof = item.prof.split(', ');
+    const lastName = prof[0];
+    const firstName = prof[1];
+
+    const [professor, professor_created] = await models.professor.findOrCreate({
+      where: {
+        firstName,
+        lastName,
+      },
+    });
+
+    // Find or create course
+    const [course, course_created] = await models.course.findOrCreate({
+      where: {
+        number: item.num,
+        prefix: item.subj,
+        semesterId: semester.id,
+      },
+    });
+
+    // Finally, we can add the section
+    const [section, section_created] = await models.section.findOrCreate({
+      where: {
+        number: item.sect,
+        grades: item.grades,
+        courseId: course.id,
+        professorId: professor.id,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    console.log(`Could not load: ${i}`);
   }
 }
 
