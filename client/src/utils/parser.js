2a2b9728e1a5f027215ffcb94c7ade547bdaaba9
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-05 12:13:37 -0500

    Fix parser

--- client/src/utils/parser.js
+++ client/src/utils/parser.js
@@ -1,26 +1,44 @@
 export default {
   parseSearchString(search) {
-    const prefixPattern = /(?<!\w)(?!summer|spring|fall)([a-zA-Z]{2,4})(?=(\s|\d+))/;
-    const numberPattern = /(?<=fall\s?|spring\s?|summer\s?)(\d{4})/;
-    const yearPattern = /(?<=fall\s|fall|spring\s|spring|summer\s|summer)(\d{4})/;
-    const semesterPattern = /(fall\s|fall|spring\s|spring|summer\s|summer)(?=\d{4})/;
-    const sectionPattern = /(?<=\d{4}|\d{4}\s|\.)(\d{1,3}|\w{1,3})(?=\s|$)/;
+    const searchString = search.toLowerCase();
 
-    const prefix = search.match(prefixPattern)[0];
-    const number = search.match(numberPattern)[0];
-    const year = search.match(yearPattern)[0];
-    const semester = search.match(semesterPattern)[0];
-    const section = search.match(sectionPattern)[0];
+    const prefixPattern = new RegExp('(?<!\\w)(?!summer|spring|fall)([a-zA-Z]{2,4})(?=(\\s|\\d+))');
+    const numberPattern = new RegExp('(?<!fall\\s?|spring\\s?|summer\\s?)(\\d{4})');
+    const yearPattern = new RegExp('(?<=fall\\s|fall|spring\\s|spring|summer\\s|summer)(\\d{4})');
+    const semesterPattern = new RegExp('(fall\\s|fall|spring\\s|spring|summer\\s|summer)(?=\\d{4})');
+    const sectionPattern = new RegExp('(?<=\\d{4}|\\d{4}\\s|\\.)(\\d{1,3}|\\w{1,3})(?=\\s|$)');
+
+    const prefix = searchString.match(prefixPattern);
+    const number = searchString.match(numberPattern);
+    const year = searchString.match(yearPattern);
+    const semester = searchString.match(semesterPattern);
+    const section = searchString.match(sectionPattern);
 
     // Professor's name?
-    const rest = search.replace(prefixPattern, '').replace(numberPattern, '').replace(yearPattern, '').replace(semesterPattern, '').replace(sectionPattern, '');
-    
-    return {
-      courseNumber: number,
-      coursePrefix: prefix,
-      sectionNumber: section,
-      type: semester,
-      year,
-    };
+    const rest = searchString.replace(prefixPattern, '').replace(numberPattern, '').replace(yearPattern, '').replace(semesterPattern, '').replace(sectionPattern, '');
+
+    let params = {};
+
+    if (number) {
+      params.courseNumber = number[0];
+    }
+
+    if (prefix) {
+      params.coursePrefix = prefix[0];
+    }
+
+    if (section) {
+      params.sectionNumber = section[0];
+    }
+
+    if (semester) {
+      params.type = semester[0];
+    }
+
+    if (year) {
+      params.year = year[0];
+    }
+
+    return params;
   }
 }
