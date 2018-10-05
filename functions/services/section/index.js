691657c3a2b57d7b8aa472fb16381765cd115020
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-04 23:31:51 -0500

    Build section queries and ignore null values

--- functions/services/section/index.js
+++ functions/services/section/index.js
@@ -1,4 +1,5 @@
 const init = require('../../models');
+const _ = require('lodash');
 
 class Section {
   constructor(sequelize) {
@@ -27,33 +28,74 @@ class Section {
     const models = this.sequelize.models;
     const Section = models.section;
 
+    function sectionWhere() {
+      let where = {};
+
+      if (!_.isNil(queryParams['sectionNumber'])) {
+        where.number = queryParams['sectionNumber'];
+      }
+
+      return where;
+    }
+
+    function professorWhere() {
+      let where = {};
+
+      if (!_.isNil(queryParams['professorFirstName'])) {
+        where.firstName = queryParams['professorFirstName'];
+      }
+
+      if (!_.isNil(queryParams['professorLastName'])) {
+        where.lastName = queryParams['professorLastName'];
+      }
+
+      return where;
+    }
+
+    function courseWhere() {
+      let where = {};
+
+      if (!_.isNil(queryParams['courseNumber'])) {
+        where.number = queryParams['courseNumber'];
+      }
+
+      if (!_.isNil(queryParams['coursePrefix'])) {
+        where.prefix = queryParams['coursePrefix'];
+      }
+      
+      return where;
+    }
+
+    function semesterWhere() {
+      let where = {};
+
+      if (!_.isNil(queryParams['year'])) {
+        where.year = queryParams['year'];
+      }
+
+      if (!_.isNil(queryParams['type'])) {
+        where.type = queryParams['type'];
+      }
+
+      return where;
+    }
+
     const section = await Section.findAll({
-      where: {
-        number: queryParams['sectionNumber'],
-      },
+      where: sectionWhere(),
       include: [
         {
           model: models.professor,
-          where: {
-            firstName: queryParams['professorFirstName'],
-            lastName: queryParams['professorLastName'],
-          },
+          where: professorWhere(),
         },
         {
           model: models.course,
-          where: {
-            number: queryParams['courseNumber'],
-            prefix: queryParams['coursePrefix'],
-          },
+          where: courseWhere(),
           include: [
             {
               model: models.semester,
-              where: {
-                year: queryParams['year'],
-                type: queryParams['type'],
-              },
-            }
-          ]
+              where: semesterWhere(),
+            },
+          ],
         },
       ],
     });
