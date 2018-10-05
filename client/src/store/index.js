8e5b03b9185390cde9d7e6cbc57820f8b99a9e95
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-05 10:45:26 -0500

    Clean up store config

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,30 +5,21 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import createReducer from 'modules/';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore(initialState = {}) {
   const middlewares = [sagaMiddleware];
-  const enhancers = [applyMiddleware(...middlewares)];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
-  /* eslint-disable no-underscore-dangle, indent */
-  const composeEnhancers =
-    process.env.NODE_ENV !== 'production' &&
-    typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-          // TODO: Try to remove when `react-router-redux` is out of beta, LOCATION_CHANGE should not be fired more than once after hot reloading
-          // Prevent recomputing reducers for `replaceReducer`
-          shouldHotReload: false,
-        })
-      : compose;
-  /* eslint-enable */
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
 
   const store = createStore(
     createReducer(),
-    composeEnhancers(...enhancers),
+    initialState,
+    composedEnhancers,
   );
 
   // Extensions
