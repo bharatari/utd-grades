4ff90d44069e37549c2fbb68a506a1e604d06f4b
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-06 18:58:21 -0500

    Structure section page

--- client/src/routes/Section/View.js
+++ client/src/routes/Section/View.js
@@ -11,15 +11,19 @@ export default class SectionView extends React.Component {
   render() {
     return (
       <Core>
-        <div className={classes.background}>
-          <div className={classes.content}>
-            <Row>
-              <Col lg={{ span: 8, offset: 8 }} sm={{ span: 18, offset: 3 }} xs={{ span: 20, offset: 2 }}>
-                <h2 className={classes.header}><span className={classes.headerBold}>UTD</span> Grades</h2>
-                { this.props.section ? <Content section={this.props.section} /> : null }
-              </Col>
-            </Row>
-          </div>
+        <Row className={classes.menu}>
+          <button className={classes.back}>
+            <Icon type="arrow-left" style={{ fontSize: '24px' }} />
+          </button>
+          <h2 className={classes.header}><span className={classes.headerBold}>UTD</span> Grades</h2>
+        </Row>
+        
+        <div className={classes.content}>
+          <Row>
+            <Col lg={{ span: 8, offset: 8 }} sm={{ span: 18, offset: 3 }} xs={{ span: 20, offset: 2 }}>
+              { this.props.section ? <Content section={this.props.section} /> : null }
+            </Col>
+          </Row>
         </div>
       </Core>
     );
--- client/src/routes/Section/components/Content/index.js
+++ client/src/routes/Section/components/Content/index.js
@@ -20,14 +20,16 @@ export default class Content extends React.Component {
     if (grades['A+']) {
       data.push({
         grade: 'A+',
-        count: parseInt(grades['A+']), 
+        count: parseInt(grades['A+']),
+        color: 'hsl(246, 70%, 50%)',
       });
     }
 
     if (grades['A']) {
       data.push({
         grade: 'A',
-        count: parseInt(grades['A']), 
+        count: parseInt(grades['A']),
+        color: 'hsl(338, 70%, 50%)',
       });
     }
 
@@ -35,83 +37,95 @@ export default class Content extends React.Component {
       data.push({
         grade: 'A-',
         count: parseInt(grades['A-']), 
+        color: 'hsl(204, 70%, 50%)',
       });
     }
 
     if (grades['B+']) {
       data.push({
         grade: 'B+',
-        count: parseInt(grades['B+']), 
+        count: parseInt(grades['B+']),
+        color: 'hsl(350, 70%, 50%)',
       });
     }
 
     if (grades['B']) {
       data.push({
         grade: 'B',
-        count: parseInt(grades['B']), 
+        count: parseInt(grades['B']),
+        color: 'hsl(199, 70%, 50%)',
       });
     }
 
     if (grades['B-']) {
       data.push({
         grade: 'B-',
-        count: parseInt(grades['B-']), 
+        count: parseInt(grades['B-']),
+        color: 'hsl(356, 70%, 50%)',
       });
     }
 
     if (grades['C+']) {
       data.push({
         grade: 'C+',
-        count: parseInt(grades['C+']), 
+        count: parseInt(grades['C+']),
+        color: '',
       });
     }
 
     if (grades['C']) {
       data.push({
         grade: 'C',
-        count: parseInt(grades['C']), 
+        count: parseInt(grades['C']),
+        color: '',
       });
     }
 
     if (grades['C-']) {
       data.push({
         grade: 'C-',
-        count: parseInt(grades['C-']), 
+        count: parseInt(grades['C-']),
+        color: '',
       });
     }
 
     if (grades['D+']) {
       data.push({
         grade: 'D+',
-        count: parseInt(grades['D+']), 
+        count: parseInt(grades['D+']),
+        color: '',
       });
     }
 
     if (grades['D']) {
       data.push({
         grade: 'D',
-        count: parseInt(grades['D']), 
+        count: parseInt(grades['D']),
+        color: '',
       });
     }
 
     if (grades['D-']) {
       data.push({
         grade: 'D-',
-        count: parseInt(grades['D-']), 
+        count: parseInt(grades['D-']),
+        color: '',
       });
     }
 
     if (grades['F']) {
       data.push({
         grade: 'F',
-        count: parseInt(grades['F']), 
+        count: parseInt(grades['F']),
+        color: '',
       });
     }
 
     if (grades['W']) {
       data.push({
         grade: 'W',
-        count: parseInt(grades['W']), 
+        count: parseInt(grades['W']),
+        color: '',
       });
     }
 
@@ -120,30 +134,77 @@ export default class Content extends React.Component {
   render() {
     return (
       <div>
-        <p>{this.props.section.number}</p>
+        <Row>
+          <h3 className={classes.header}>{this.props.section.course.prefix} {this.props.section.course.number}<span className={classes.section}>.{this.props.section.number}</span></h3>
+          <h5 className={classes.subheader}>{this.props.section.professor.lastName}, {this.props.section.professor.firstName}</h5>
+        </Row>
 
-        <Bar
-          width={900}
-          height={500}
-          margin={{
-            top: 60,
-            right: 80,
-            bottom: 60,
-            left: 80
-          }}
-          data={this.transformData(this.props.section.grades)}
-          indexBy="grade"
-          keys={[
-            'count',
-          ]}
-          padding={0.2}
-          labelTextColor="inherit:darker(1.4)"
-          labelSkipWidth={16}
-          labelSkipHeight={16}
+        <Row>
+          <div className={classes.graphContainer}>
+            <Bar
+              defs={[
+                {
+                  "id": "dots",
+                  "type": "patternDots",
+                  "background": "inherit",
+                  "color": "#38bcb2",
+                  "size": 4,
+                  "padding": 1,
+                  "stagger": true
+                },
+                {
+                  "id": "lines",
+                  "type": "patternLines",
+                  "background": "inherit",
+                  "color": "#eed312",
+                  "rotation": -45,
+                  "lineWidth": 6,
+                  "spacing": 10
+                }
+              ]}
+              fill={[
+                {
+                  "match": {
+                      "grade": "A"
+                  },
+                  "id": "dots"
+                },
+                {
+                  "match": {
+                      "grade": "B"
+                  },
+                  "id": "lines"
+                }
+              ]}
+              width={800}
+              height={500}
+              margin={{
+                top: 60,
+                right: 80,
+                bottom: 60,
+                left: 80
+              }}
+              data={this.transformData(this.props.section.grades)}
+              indexBy="grade"
+              colors="nivo"
+              colorBy="color"
+              keys={[
+                'count',
+              ]}
+              padding={0.2}
+              labelTextColor="inherit:darker(1.4)"
+              labelSkipWidth={16}
+              labelSkipHeight={16}
+              animate={true}
+              isInteractive={false}
+              theme={{tooltip: {container: {background: '#333'}}}}
+            />
+          </div>
+        </Row>
 
-          
-          theme={{tooltip: {container: {background: '#333'}}}}
-        />
+        <Row className={classes.otherSectionsRow}>
+          <p className={classes.otherSectionsHeader}>Other Sections</p>
+        </Row>
       </div>
     );
   }
