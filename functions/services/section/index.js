a8d69a5cbc2bdd091537a60760b3e3d4e67f91cd
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-06 16:07:34 -0500

    Close sequelize connection before response

--- functions/services/section/index.js
+++ functions/services/section/index.js
@@ -7,106 +7,122 @@ class Section {
   }
   
   async get(id) {
-    const models = this.sequelize.models;
-    const Section = models.section;
+    try {
+      const models = this.sequelize.models;
+      const Section = models.section;
 
-    const section = await Section.findOne({
-      where: {
-        id,
-      },
-      include: [
-        {
-          model: models.professor
+      const section = await Section.findOne({
+        where: {
+          id,
         },
-        {
-          model: models.course,
-          include: [
-            { model: models.semester },
-          ],
-        },
-      ],
-    });
+        include: [
+          {
+            model: models.professor
+          },
+          {
+            model: models.course,
+            include: [
+              { model: models.semester },
+            ],
+          },
+        ],
+      });
 
-    return section;
+      this.sequelize.close();
+
+      return section;
+    } catch (e) {
+      this.sequelize.close();
+
+      throw e;
+    }
   }
 
   async find(queryParams) {
-    const models = this.sequelize.models;
-    const Section = models.section;
+    try {
+      const models = this.sequelize.models;
+      const Section = models.section;
 
-    function sectionWhere() {
-      let where = {};
+      function sectionWhere() {
+        let where = {};
 
-      if (!_.isNil(queryParams['sectionNumber'])) {
-        where.number = queryParams['sectionNumber'].trim();
+        if (!_.isNil(queryParams['sectionNumber'])) {
+          where.number = queryParams['sectionNumber'].trim();
+        }
+
+        return where;
       }
 
-      return where;
+      function professorWhere() {
+        let where = {};
+
+        if (!_.isNil(queryParams['professorFirstName'])) {
+          where.firstName = queryParams['professorFirstName'].trim();
+        }
+
+        if (!_.isNil(queryParams['professorLastName'])) {
+          where.lastName = queryParams['professorLastName'].trim();
+        }
+
+        return where;
+      }
+
+      function courseWhere() {
+        let where = {};
+
+        if (!_.isNil(queryParams['courseNumber'])) {
+          where.number = queryParams['courseNumber'].trim();
+        }
+
+        if (!_.isNil(queryParams['coursePrefix'])) {
+          where.prefix = queryParams['coursePrefix'].toUpperCase().trim();
+        }
+        
+        return where;
+      }
+
+      function semesterWhere() {
+        let where = {};
+
+        if (!_.isNil(queryParams['year'])) {
+          where.year = queryParams['year'].trim();
+        }
+
+        if (!_.isNil(queryParams['type'])) {
+          where.type = queryParams['type'].toLowerCase().trim();
+        }
+
+        return where;
+      }
+
+      const sections = await Section.findAll({
+        where: sectionWhere(),
+        include: [
+          {
+            model: models.professor,
+            where: professorWhere(),
+          },
+          {
+            model: models.course,
+            where: courseWhere(),
+            include: [
+              {
+                model: models.semester,
+                where: semesterWhere(),
+              },
+            ],
+          },
+        ],
+      });
+
+      this.sequelize.close();
+
+      return sections;
+    } catch (e) {
+      this.sequelize.close();
+
+      throw e;
     }
-
-    function professorWhere() {
-      let where = {};
-
-      if (!_.isNil(queryParams['professorFirstName'])) {
-        where.firstName = queryParams['professorFirstName'].trim();
-      }
-
-      if (!_.isNil(queryParams['professorLastName'])) {
-        where.lastName = queryParams['professorLastName'].trim();
-      }
-
-      return where;
-    }
-
-    function courseWhere() {
-      let where = {};
-
-      if (!_.isNil(queryParams['courseNumber'])) {
-        where.number = queryParams['courseNumber'].trim();
-      }
-
-      if (!_.isNil(queryParams['coursePrefix'])) {
-        where.prefix = queryParams['coursePrefix'].toUpperCase().trim();
-      }
-      
-      return where;
-    }
-
-    function semesterWhere() {
-      let where = {};
-
-      if (!_.isNil(queryParams['year'])) {
-        where.year = queryParams['year'].trim();
-      }
-
-      if (!_.isNil(queryParams['type'])) {
-        where.type = queryParams['type'].toLowerCase().trim();
-      }
-
-      return where;
-    }
-
-    const section = await Section.findAll({
-      where: sectionWhere(),
-      include: [
-        {
-          model: models.professor,
-          where: professorWhere(),
-        },
-        {
-          model: models.course,
-          where: courseWhere(),
-          include: [
-            {
-              model: models.semester,
-              where: semesterWhere(),
-            },
-          ],
-        },
-      ],
-    });
-
-    return section;
   }
 }
 
