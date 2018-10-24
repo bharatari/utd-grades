89f18bd814736a4c911283f087f2ea13cea7364d
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-24 14:14:16 -0500

    Replace nivo with chart.js

--- client/src/routes/Section/components/Content/index.js
+++ client/src/routes/Section/components/Content/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import classes from './styles.scss';
-import { Core } from 'components/';
+import { Core, Graph } from 'components/';
 import { Row, Col, Card, Icon, Popover } from 'antd';
-import { Bar } from '@nivo/bar';
 
 export default class Content extends React.Component {
   transformData = (grades) => {
@@ -141,69 +140,13 @@ export default class Content extends React.Component {
 
         <Row>
           <div className={classes.graphContainer}>
-            <Bar
-              defs={[
-                {
-                  "id": "dots",
-                  "type": "patternDots",
-                  "background": "inherit",
-                  "color": "#38bcb2",
-                  "size": 4,
-                  "padding": 1,
-                  "stagger": true
-                },
-                {
-                  "id": "lines",
-                  "type": "patternLines",
-                  "background": "inherit",
-                  "color": "#eed312",
-                  "rotation": -45,
-                  "lineWidth": 6,
-                  "spacing": 10
-                }
-              ]}
-              fill={[
-                {
-                  "match": {
-                      "grade": "A"
-                  },
-                  "id": "dots"
-                },
-                {
-                  "match": {
-                      "grade": "B"
-                  },
-                  "id": "lines"
-                }
-              ]}
-              width={800}
-              height={500}
-              margin={{
-                top: 60,
-                right: 80,
-                bottom: 60,
-                left: 80
-              }}
-              data={this.transformData(this.props.section.grades)}
-              indexBy="grade"
-              colors="nivo"
-              colorBy="color"
-              keys={[
-                'count',
-              ]}
-              padding={0.2}
-              labelTextColor="inherit:darker(1.4)"
-              labelSkipWidth={16}
-              labelSkipHeight={16}
-              animate={true}
-              isInteractive={false}
-              theme={{tooltip: {container: {background: '#333'}}}}
-            />
+            <Graph type="bar" data={{ labels: ['A', 'B'], datasets: [{ backgroundColor: 'hsl(350, 70%, 50%)', data: [1,2] }]}} />
           </div>
         </Row>
 
         <Row className={classes.otherSectionsRow}>
           <p className={classes.otherSectionsHeader}>Other Sections</p>
+          
         </Row>
       </div>
     );
