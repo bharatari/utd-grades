6cf2fcdc67da5fce4d444fb802f8ea79fbd03e5c
Bharat Arimilli <bbarimilli@outlook.com>
2018-10-25 14:22:24 -0500

    Add links to footer

--- client/src/components/Core/index.js
+++ client/src/components/Core/index.js
@@ -16,7 +16,7 @@ function Core({ children }) {
       </div>
       <div className={classes.footer}>
         <p>Built with <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" /> by <a href="https://www.acmutd.co">ACM Labs</a> and powered <Icon type="thunderbolt" theme="twoTone" twoToneColor="#ffcc00" /> by <a href="https://www.utdmercury.com">The Mercury</a>.</p>
-        <p>Designed by <a href="https://www.arimilli.io">Bharat Arimilli</a>. Thanks to Garrett Gu, Jeffrey Wang, Joseph Wickline and our <Popover content={donors}><span style={{ textDecoration: 'underline' }}>donors</span>.</Popover></p>
+        <p>Designed by <a href="https://www.arimilli.io">Bharat Arimilli</a>. Thanks to <a href="https://garrettgu.com/">Garrett Gu</a>, <a href="https://jeffw.xyz/">Jeffrey Wang</a>, <a href="https://www.linkedin.com/in/josephwickline/">Joseph Wickline</a> and our <Popover content={donors}><span style={{ textDecoration: 'underline' }}>donors</span>.</Popover></p>
       </div>
     </div>
   );
