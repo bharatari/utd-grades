4160498c05c9d124dcc3c09c5aa598742206448b
Bharat Arimilli <bbarimilli@outlook.com>
2020-04-25 22:34:29 -0500

    Add comment regarding temporary fix

--- client/pages/results.js
+++ client/pages/results.js
@@ -13,6 +13,9 @@ export default function Results() {
 
   const { search, sectionId } = router.query;
 
+  // Query params not populated on first render in Next.js
+  // Migrate to checking Router.ready when available
+  // https://github.com/zeit/next.js/issues/8259
   if (router.asPath !== router.route) {
     return (
       <Core>
