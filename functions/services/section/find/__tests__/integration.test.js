a243be54e3372bbaa6b6e5b1b84f0a8b8671e295
Bharat Arimilli <bbarimilli@outlook.com>
2020-04-26 15:36:16 -0500

    Get tests working

--- functions/services/section/find/__tests__/integration.test.js
+++ functions/services/section/find/__tests__/integration.test.js
@@ -14,7 +14,7 @@ describe('Test sample queries', () => {
     const queries = [
       { search: 'CS 1337', expected: { coursePrefix: 'CS', courseNumber: '1337' } },
       { search: 'BCOM 3310', expected: { coursePrefix: 'BCOM', courseNumber: '3310' } },
-      { search: 'HIST 1301', expected: { coursePrefix: 'BCOM', courseNumber: '1301' } }
+      { search: 'HIST 1301', expected: { coursePrefix: 'HIST', courseNumber: '1301' } }
     ];
 
     for (let i = 0; i < queries.length; i++) {
@@ -31,7 +31,7 @@ describe('Test sample queries', () => {
     const queries = [
       { search: 'cs 1337', expected: { coursePrefix: 'CS', courseNumber: '1337' } },
       { search: 'bcom 3310', expected: { coursePrefix: 'BCOM', courseNumber: '3310' } },
-      { search: 'hist 1301', expected: { coursePrefix: 'BCOM', courseNumber: '1301' } }
+      { search: 'hist 1301', expected: { coursePrefix: 'HIST', courseNumber: '1301' } }
     ];
 
     for (let i = 0; i < queries.length; i++) {
@@ -49,7 +49,7 @@ describe('Test sample queries', () => {
       { search: 'CS 1337.001', expected: { coursePrefix: 'CS', courseNumber: '1337', sectionNumber:'1' } },
       { search: 'CS 1337.0U1', expected: { coursePrefix: 'CS', courseNumber: '1337', sectionNumber:'0U1' } },
       { search: 'BCOM 3310.HON', expected: { coursePrefix: 'BCOM', courseNumber: '3310', sectionNumber: 'HON' } },
-      { search: 'HIST 1301 001', expected: { coursePrefix: 'BCOM', courseNumber: '1301', sectionNumber: '1'} }
+      { search: 'HIST 1301 001', expected: { coursePrefix: 'HIST', courseNumber: '1301', sectionNumber: '1'} }
     ];
 
     for (let i = 0; i < queries.length; i++) {
@@ -59,7 +59,7 @@ describe('Test sample queries', () => {
       expect(response).not.toHaveLength(0);
       expect(response[0].course.number).toEqual(expected.courseNumber);
       expect(response[0].course.prefix).toEqual(expected.coursePrefix);
-      expect(response[0].number).stringContaining(expected.sectionNumber);
+      expect(response[0].number.includes(expected.sectionNumber)).toBeTruthy();
     }
   });
 
@@ -67,7 +67,7 @@ describe('Test sample queries', () => {
     const queries = [
       { search: 'CS 1337 fall 2019', expected: { coursePrefix: 'CS', courseNumber: '1337', semesterType: 'fall', year: 2019 } },
       { search: 'BCOM 3310 Summer 2019', expected: { coursePrefix: 'BCOM', courseNumber: '3310', semesterType: 'summer', year: 2019 } },
-      { search: 'HIST 1301 Spring 2018', expected: { coursePrefix: 'BCOM', courseNumber: '1301', semesterType: 'spring', year: 2018 } }
+      { search: 'HIST 1301 Spring 2018', expected: { coursePrefix: 'HIST', courseNumber: '1301', semesterType: 'spring', year: 2018 } }
     ];
 
     for (let i = 0; i < queries.length; i++) {
@@ -84,7 +84,10 @@ describe('Test sample queries', () => {
 
   test('Should handle professor names', async () => {
     const queries = [
-      { search: 'CS 1337 fall 2019', expected: { coursePrefix: 'CS', courseNumber: '1337', professorFirstName: 'Stephen', professorLastName: 'Perkins' } }
+      { search: 'CS 1337 Jason Smith', expected: { coursePrefix: 'CS', courseNumber: '1337', professorFirstName: 'Jason W', professorLastName: 'Smith' } },
+      { search: 'CS 1337 jason smith', expected: { coursePrefix: 'CS', courseNumber: '1337', professorFirstName: 'Jason W', professorLastName: 'Smith' } },
+      { search: 'CS 1337 Cole', expected: { coursePrefix: 'CS', courseNumber: '1337', professorFirstName: 'John', professorLastName: 'Cole' } },
+      { search: 'CS 1337 cole', expected: { coursePrefix: 'CS', courseNumber: '1337', professorFirstName: 'John', professorLastName: 'Cole' } }
     ];
 
     for (let i = 0; i < queries.length; i++) {
@@ -101,8 +104,8 @@ describe('Test sample queries', () => {
 
   test('Should handle everything together', async () => {
     const queries = [
-      { search: 'CS 1337 501 fall 2019 Stephen Perkins', expected: { coursePrefix: 'CS', courseNumber: '1337', semesterType: 'fall', year: 2019 } },
-      { search: 'BCOM 3310 HON Fall 2019 Stephen'}
+      { search: 'CS 1337 502 fall 2019 Stephen Perkins', expected: { coursePrefix: 'CS', courseNumber: '1337', sectionNumber: '501', semesterType: 'fall', year: 2019, professorFirstName: 'Stephen J', professorLastName: 'Perkins' } },
+      { search: 'BCOM 3310 HON Fall 2019 Kristen Lawson', expected: { coursePrefix: 'BCOM', courseNumber: '3310', sectionNumber: 'HON', semesterType: 'fall', year: 2019, professorFirstName: 'Kristen A', professorLastName: 'Lawson' } }
     ];
 
     for (let i = 0; i < queries.length; i++) {
@@ -114,6 +117,14 @@ describe('Test sample queries', () => {
       expect(response[0].course.prefix).toEqual(expected.coursePrefix);
       expect(response[0].course.semester.type).toEqual(expected.semesterType);
       expect(response[0].course.semester.year).toEqual(expected.year);
+      expect(response[0].professor.firstName).toEqual(expected.professorFirstName);
+      expect(response[0].professor.lastName).toEqual(expected.professorLastName);
     }
   });
+});
+
+afterAll(async () => {
+  connection = new Connection();
+
+  await connection.close();
 });
